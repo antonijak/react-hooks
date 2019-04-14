7893bcec8a2d7b234a7fbfb05644d3fe3b3a49bf
Antonija K <antonija.kasum@gmail.com>
2019-04-14 23:22:22 +0300

    useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const Context = React.createContext();
+
+const Parent = props => {
+  const obj = { text: 'random text' };
+  return <Context.Provider value={obj}>{props.children}</Context.Provider>;
+};
+const Child = () => {
+  const data = React.useContext(Context);
+  return <div style={{ margin: '50vh auto' }}>{data.text}</div>;
+};
+
+const App = () => {
+  const [count, updateCount] = useState(0);
+  // useEffect(() => {
+  //   // Update the document title using the browser API
+  //   // document.title = `You clicked ${count} times`;
+  //   console.log('useEff');
+  //   updateCount(count + 1);
+  //   return () => updateCount(count - 1);
+  // });
+  useEffect(() => {
+    console.log('mount');
+    return () => {
+      console.log('unmount');
+      // updateCount(0);
+    };
+  }, [count]);
+  console.log('render');
+  return (
+    <div className="App">
+      {/* <p>{count}</p>
+      <button
+        onClick={() => {
+          console.log('update count');
+          updateCount(count + 1);
+        }}
+      >
+        Add
+      </button> */}
+      <Parent>
+        <Child />
+      </Parent>
+    </div>
+  );
+};
 
 export default App;
